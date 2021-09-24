4317a98194acce9dbd590c22899733f330f08a92
Ivan Yeung <iyeung30@stuy.edu>
2021-09-23 21:01:35 -0400

    Actually created the celsiusToFahrenheit() function this time

--- CtoFTester.java
+++ CtoFTester.java
@@ -8,4 +8,8 @@ public class CtoFTester {
   public static void main(String[] args) {
 
   }
+  public static double celsiusToFahrenheit(double celsius) {
+    final double conversionConstant = 9.0/5.0;
+    return (celsius * conversionConstant) + 32.0;
+  }
 }
