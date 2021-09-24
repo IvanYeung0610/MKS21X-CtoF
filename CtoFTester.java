a6a810ef6abb1cc715ade4d9c690df87378c1c25
Ivan Yeung <iyeung30@stuy.edu>
2021-09-23 20:49:53 -0400

    Answered the questions and created the main method.

---
+++ CtoFTester.java
@@ -0,0 +1,11 @@
+/**
+The parameters should either be the degrees in Celsius or Fahrenheit. They should be double type.
+The returnvalue type is double for both functions. The functions should return the temperature in Celsisus or
+Fahrenheit depending on the function. (celsiusToFahrenheit() should return Fahrenheit while fahrenheitToCelsius() should return
+Celsius.)
+**/
+public class CtoFTester {
+  public static void main(String[] args) {
+
+  }
+}
